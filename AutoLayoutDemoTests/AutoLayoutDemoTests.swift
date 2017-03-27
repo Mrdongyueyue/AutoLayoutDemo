7d3f4f8460304125a243d8930f5497c1730078a2
董知樾 <dongzhiyue@dongzhiyuedeMacBook-Pro.local>
2017-03-27 15:16:11 +0800

    Initial Commit

---
+++ AutoLayoutDemoTests/AutoLayoutDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  AutoLayoutDemoTests.swift
+//  AutoLayoutDemoTests
+//
+//  Created by 董知樾 on 2017/3/27.
+//  Copyright © 2017年 董知樾. All rights reserved.
+//
+
+import XCTest
+@testable import AutoLayoutDemo
+
+class AutoLayoutDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
